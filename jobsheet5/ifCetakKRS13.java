312ed84a7cef8e83f1adf027162791018671937f
Moch.Agung Bima Ardian <bimaagungbimzz@gmail.com>
2025-10-12 20:56:54 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS13.java
+++ jobsheet5/ifCetakKRS13.java
@@ -5,21 +5,14 @@ import java.util.Scanner;
 public class ifCetakKRS13 {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-    System.out.println("--- Cetak KRS SIAKAD ---");
+    
+        System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (true/false: ");
         boolean uktLunas= sc.nextBoolean();
+      
+        String pesan = (uktLunas) ? "Pembayaran UKT terverifikasi. Silakan cetas KRS dan minta tanda tangan DPA." : "Regustrasi ditolak. Silakan lunasi UKT terlebih dahulu.";
+        System.out.println(pesan);
 
-
-        if (uktLunas) {
-        System.out.println("Pembayaran UKT terverifikasi");
-        System.out.println("Silakan cetas KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
-
-              boolean presensi = false;
-              String hasil = presensi ? "Cetak KRS diperbolehkan" : "Tidak boleh cetak KRS";
-              System.out.println(hasil);
         }
 
     }
-}
